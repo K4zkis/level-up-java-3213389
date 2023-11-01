8f06de7d1f4fe142357c91ab2ae0676514a254e7
Janus Milojevic <69076805+K4zkis@users.noreply.github.com>
2023-11-01 18:53:21 +0000

    Refactoring the Doublke or nothing game

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
@@ -10,28 +10,41 @@ public class DoubleOrNothing {
   public void playGame() {
     int points = 10;
     boolean quitGame = false;
+    String continueInput = "y";
     Scanner scanner = new Scanner(System.in);
       
     while(quitGame != true) {
     
-    System.out.println("your current points are: "+ points);  
-    System.out.println("You wanna 'double or nothing' or walk away? (y/n)");
-
-    String userInput = scanner.nextLine();
-    
-    if (userInput.equals("y")){
-      if (Math.random()<0.5){
-        points = points*2;
+      gameStartMessage(points);
+      String userInput = scanner.nextLine();
+      
+      if (userInput.equals(continueInput)){
+        points = gamblePoints(points);
       }
       else {
-        points = 0;
+        gameExitMessage(points);
+        quitGame = true;
       }
-    }
-    else {
-      System.out.println("final points = "+points);
-      quitGame = true;
-    }
   }
   scanner.close();
 }
+
+  private void gameExitMessage(int points) {
+    System.out.println("final points = "+points);
+  }
+
+  private int gamblePoints(int points) {
+    if (Math.random()<0.5){
+      points = points*2;
+    }
+    else {
+      points = 0;
+    }
+    return points;
+  }
+
+  private void gameStartMessage(int points) {
+    System.out.println("your current points are: "+ points);  
+    System.out.println("You wanna 'double or nothing' or walk away? (y/n)");
+  }
 };
