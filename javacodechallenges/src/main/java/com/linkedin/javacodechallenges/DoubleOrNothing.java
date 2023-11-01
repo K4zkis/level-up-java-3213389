52bda7bbf535d5328b330685c511cc8739996150
Janus Milojevic <69076805+K4zkis@users.noreply.github.com>
2023-11-01 17:05:38 +0000

    implementing doublie or nothing logic

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
@@ -1,6 +1,37 @@
+package com.linkedin.javacodechallenges;
+
+import java.util.Scanner;
+
 public class DoubleOrNothing {
+  
+
+  
 
   public void playGame() {
-    // TODO: Implement method
+    int points = 10;
+    boolean quitGame = false;
+    Scanner scanner = new Scanner(System.in);
+      
+    while(quitGame != true) {
+    
+    System.out.println("your current points are: "+ points);  
+    System.out.println("You wanna 'double or nothing' or walk away? (y/n)");
+
+    String userInput = scanner.nextLine();
+    
+    if (userInput.equals("y")){
+      if (Math.random()<0.5){
+        points = points*2;
+      }
+      else {
+        points = 0;
+      }
+    }
+    else {
+      System.out.println("final points = "+points);
+      quitGame = true;
+    }
   }
+  scanner.close();
 }
+};
