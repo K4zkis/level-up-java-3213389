6416e92c1f195b40766b3cafaf79c9de2f75337e
Janus.Milojevic <Janus.Milojevic@fdmgroup.com>
2023-11-02 23:06:12 +0100

    implementing person model

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -4,6 +4,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        
+       Person p1 = new Person("Someone", "who",27);
+       Person p2 = new Person("isn't", "me", 35);
+       Person p3 = new Person("aka", "swim", 42); 
+
+       p1.introduction();
+       p2.introduction();
+       p3.introduction();
     }
 }
--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
@@ -1,3 +1,38 @@
+package com.linkedin.javacodechallenges;
+
 public class Person {
-  
+
+  private String firstName;
+  private String lastName;
+  private int age;
+
+  public Person(String firstName, String lastName, int age){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  public void setFirstName(String firstName){
+    this.firstName = firstName;
+  }
+  public String getFirstName(){
+    return this.firstName;
+  }
+  public void setLastName(String lastName){
+    this.lastName = lastName;
+  }
+  public String getLastName(){
+    return this.lastName;
+  }
+  public void setAge(int age){
+    this.age=age;
+  }
+  public int getAge(){
+    return this.age;
+  }
+  public void introduction(){
+    System.out.println("Hello my name is "
+    +this.firstName+" "+this.lastName
+    +" and I am "+this.age+" years old.");
+  }
 }
