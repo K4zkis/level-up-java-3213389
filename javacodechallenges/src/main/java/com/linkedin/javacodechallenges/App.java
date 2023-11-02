3aaa5a54af0685340112077e303e32706b48cd39
Janus.Milojevic <Janus.Milojevic@fdmgroup.com>
2023-11-02 19:03:14 +0100

    add water Bill calculation logic

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,10 +5,34 @@ import java.util.Scanner;
 public class App {
 
     public static double calculateWaterBill(double gallonsUsage) {
-        // TODO: Implement method
+                int measuriungUnitofGallons = 748;
+        double pricePerMeasuringUnit = 3.90;
+        int minimumGallonUsage = 2*measuriungUnitofGallons;
+        double minimumPrice = 18.84;
+        double priceToPay = 0;
+
+        priceToPay = minimumPrice;
+        
+
+        if (gallonsUsage > minimumGallonUsage){
+            gallonsUsage -= minimumGallonUsage;
+            double amountOfUnitsUsed = gallonsUsage/measuriungUnitofGallons;
+            double fullUnitsUsed = Math.round(amountOfUnitsUsed);
+
+            priceToPay = addFractionalUnitUsed(pricePerMeasuringUnit, priceToPay, amountOfUnitsUsed);
+            priceToPay += fullUnitsUsed*pricePerMeasuringUnit;
+        }
+        return (Math.round(priceToPay*100))/100.0;
         return 0;
     }
 
+    private static double addFractionalUnitUsed(double pricePerMeasuringUnit, double priceToPay, double amountOfUnitsUsed) {
+        if (amountOfUnitsUsed > Math.round(amountOfUnitsUsed)){
+            priceToPay += pricePerMeasuringUnit;
+        }
+        return priceToPay;
+    }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many gallons of water did you " +
